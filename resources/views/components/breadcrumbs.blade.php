4354911789ffb2863e7a607654e628e00e3377ae
SiapaLupa <muhammadilyasafadhlih@gmail.com>
2023-06-28 17:00:00 +0700

    fix breadcrumbs

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,10 +1,12 @@
 <nav aria-label="breadcrumb">
+    @php($path = asset('admin'))
     <ol class="breadcrumb">
         @foreach ($routeParameters as $param)
         @if ($loop->last)
         <li class="breadcrumb-item active">{{ $param }}</li>
         @else
-        <li class="breadcrumb-item"><a href="#">{{ $param }}</a></li>
+        <li class="breadcrumb-item"><a href="{{ $path = implode('/', [$path, $param]) }}">{{ $param
+                }}</a></li>
         @endif
         @endforeach
     </ol>
